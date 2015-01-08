7771ed9bcd34fe81905f76b5b678e94a58aca00a
kuhe <kuhe@users.noreply.github.com>
2015-01-08 15:57:10 -0500

    Update Score_ActiveRecord_Overlay.cs

--- C#-ActiveRecord/Models/Score_ActiveRecord_Overlay.cs
+++ C#-ActiveRecord/Models/Score_ActiveRecord_Overlay.cs
@@ -4,6 +4,7 @@ namespace WebApplication1.Models
 {
     public partial class Score : ActiveRecord<Score>
     {
+        // note: delete the property Id from the generated partial class definition
         protected override void Initialize()
         {
             FlashCardEntities context = new FlashCardEntities();
