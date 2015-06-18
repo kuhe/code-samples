68b7cbc46568b94e41c8a286623203e4a803e0c4
George Fu <gjf@foundationcenter.org>
2015-06-18 12:53:47 -0400

    minor formatting

--- C++ Learning Reference/main.cpp
+++ C++ Learning Reference/main.cpp
@@ -14,28 +14,22 @@ int main() {
     int test = test_structures();
     assert(0 == test);
 
-    bool runLegacy = false;
-
-    if (runLegacy) {
+    bool runOldSolutions = 0;
+    if (runOldSolutions) {
         GreedyScheduler gs;
         string solution1 = gs.solve();
         cout << "Weight - length: " + solution1 << endl;
-
         string solution2 = gs.solve(true);
         cout << "weight / length: " + solution2 << endl;
-
         Prim prim;
         string solution3 = prim.solve();
         cout << "Prim's: " + solution3 << endl;
     }
-
-    bool runIncompleteSolutions = false;
-
+    bool runIncompleteSolutions = 0;
     if (runIncompleteSolutions) {
         DistanceClustering dc;
         string solution4 = dc.solve(4);
         cout << "Distance clustering: " + solution4 << endl;
-
         DistanceClustering dc2;
         string solution5 = dc2.solveHamming();
         cout << "Large Hamming cluster: " + solution5 << endl;
