d9e77ed09f4d2b03c420f1382c25aa89aebfea83
George Fu <gjf@foundationcenter.org>
2015-07-14 15:51:33 -0400

    key collision handling

--- C++ Learning Reference/Data Structures/Map/Map.h
+++ C++ Learning Reference/Data Structures/Map/Map.h
@@ -9,22 +9,44 @@ namespace Lehr {
     class KeyValuePair {
     public:
         KeyValuePair() {}
-        KeyValuePair(const K& key, const V& value) : key(key), value(value) {}
+        KeyValuePair(const K& key) : key(key) {
+            initialized = true;
+        }
+        KeyValuePair(const K& key, const V& value) : key(key), value(value) {
+            initialized = true;
+        }
         K key;
         V value;
+        ~KeyValuePair() {
+            if (nullptr != next) {
+                delete next;
+            }
+        }
         operator KeyValuePair() {
             return value;
         }
         void operator =(V& value) {
-            this->value = value;
+            setValue(value);
         }
         void operator =(V* value) {
-            this->value = *value;
+            setValue(*value);
         }
         void operator =(V value) {
-            this->value = value;
+            setValue(value);
         }
         KeyValuePair* next = nullptr;
+        bool isInitialized() {
+            return initialized;
+        }
+        void initialize() {
+            initialized = true;
+        }
+    private:
+        void setValue(V& value) {
+            initialized = true;
+            this->value = value;
+        }
+        bool initialized = false;
     };
 
     template <typename K>
@@ -56,7 +78,7 @@ namespace Lehr {
     template <typename K, typename V>
     class Map {
     public:
-        Map() {
+        Map(int size = 128) : tableSize(size) {
             table = new KeyValuePair<K,V>[tableSize];
         }
 
@@ -77,17 +99,27 @@ namespace Lehr {
                 }
             } while(candidate->next != nullptr);
             if (candidate->key != key) {
-                candidate->key = key;
+                if (candidate->isInitialized()) {
+                    candidate->next = new KeyValuePair<K,V>(key);
+                    candidate = candidate->next;
+                } else {
+                    candidate->key = key;
+                    candidate->initialize();
+                }
             }
             return candidate->value;
+        }
+        int hash(const K& key) const {
+            Hash<K> h(tableSize);
+            return h(&key);
+        }
+        int hash(const K* key) const {
+            Hash<K> h(tableSize);
+            return h(key);
         }
     private:
         int tableSize = 128;
         KeyValuePair<K,V>* table;
-        int hash(const K* key) const {
-            Hash<K> h(128);
-            return h(key);
-        }
     };
 }
 
--- C++ Learning Reference/Tests/test_map.cpp
+++ C++ Learning Reference/Tests/test_map.cpp
@@ -5,19 +5,48 @@ using Lehr::List;
 using Lehr::Hash;
 using Lehr::KeyValuePair;
 
+template <typename T, typename V>
+void test(T a, V b) {
+    cout << a << " " << b << endl;
+}
+template void test<string, string>(string a, string b);
+
 int test_map() {
+    string a = "hello.",
+           b = "how.",
+           c = "are.",
+           d = "you.",
+           e = "my.",
+           f = "name.",
+           g = "is.",
+           t1, t2;
 
-    Map<string, string> map;
-
+    /** test collision */
+    Map<string, string> map(1);
+    test(map.hash(a), map.hash(a));
+    test(map.hash(b), map.hash(b));
+    test(map.hash(g), map.hash(g));
     map["greeting"] = "hello";
+    map["phrase"] = "how are you";
+    map["phrase_two"] = "but I\'m hopin\' all the same";
+    t1 = map["greeting"];
+    t2 = map["greeting"];
+    map["phrase"] = "I don\'t even know your name";
+    test(t1, t2);
+    t2 = "I don\'t even know your name";
+    test(map["phrase"], t2);
 
-    string test = map["greeting"];
-
-    map["greeting"] = "g\'day";
-
-    string test1 = map["greeting"];
-
-    cout << (test) << endl << (test1) << endl;
+    /** test with higher size */
+    Map<string, string> map2(1024);
+    map2["greeting"] = "hello";
+    map2["phrase"] = "how are you";
+    map2["phrase_two"] = "but I\'m hopin\' all the same";
+    t1 = map2["greeting"];
+    t2 = map2["greeting"];
+    map2["phrase"] = "I don\'t even know your name";
+    test(t1, t2);
+    t2 = "I don\'t even know your name";
+    test(map["phrase"], t2);
 
     return 0;
 };
