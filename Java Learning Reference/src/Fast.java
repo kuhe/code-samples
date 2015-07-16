d8fbb69cfdfd3176ec26242c5e2373b792f2e48f
George Fu <gjf@foundationcenter.org>
2015-07-16 11:29:44 -0400

    euclidean

---
+++ Java Learning Reference/src/Fast.java
@@ -0,0 +1,8 @@
+public class Fast {
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            throw new IllegalArgumentException();
+        }
+        String filename = args[0];
+    }
+}
