49b7c402bb26c88b9b5112a33b241f26df89c82a
gefu <George.Fu@mlb.com>
2016-03-31 11:11:08 -0400

    rval destructor mysetry solved

--- C++ Learning Reference/Tests/test_rule_of_X.cpp
+++ C++ Learning Reference/Tests/test_rule_of_X.cpp
@@ -8,18 +8,19 @@ void test_rule_of_X() {
     Q q2((int)q1);
     Q q3 = q2;
     Q q4 = std::move(q3);
-    console_test(0, results.dtor_count);
 
     {
         Q q5;
-        q5 = q4;
+        q5 = q4; // copy assign
         Q q6;
-        q6 = 5 + (int) q5;
+        console_test(0, results.dtor_count);
+        q6 = 5 + (int) q5; // move assign, the Q rval cast from 5 was destroyed in the same line
+        console_test(1, results.dtor_count);
         console_test(q5.state, Q::assign_copy);
         console_test(q6.state, Q::assign_move);
     } // q5, q6 destroyed
 
-    console_test(2, results.dtor_count); // todo: hmm where is the 3rd destructor call?
+    console_test(3, results.dtor_count); // tricky 3rd destructor call!
     console_test(q1point5.state, Q::ctor);
     console_test(q1.state, Q::ctor);
     console_test(q2.state, Q::ctor_param);
